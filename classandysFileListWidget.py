d82eac02e5bc7d0cce88597363151829b5bdb24c
wangandi520 <wangandi520@gmail.com>
2022-03-23 00:11:49 +0800

    Update classandysFileListWidget.py

--- classandysFileListWidget.py
+++ classandysFileListWidget.py
@@ -6,13 +6,14 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QListWidget, QPushButton, QFileDialog
 from pathlib import Path
 
+
 # pip install pyqt5
 
 class andysFileListWidget(QWidget):
     def __init__(self, parent=None):
         # 拖拽文件加入列表。如果拖拽文件夹，里面的所有文件都加入。不重复加入
         super().__init__(parent)
-        
+
         self.setAcceptDrops(True)
         self.allFileListArray = []
         self.fileListWidget = QListWidget()
@@ -20,27 +21,26 @@ class andysFileListWidget(QWidget):
         deleteFileButton = QPushButton('删除')
         clearListButton = QPushButton('清空')
 
-        #self.fileListWidget.currentRowChanged.connect(self.doCurrentRowChanged)
+        self.fileListWidget.currentRowChanged.connect(self.doCurrentRowChanged)
+        
         openFileButton.clicked.connect(self.doOpenFileButton)
         deleteFileButton.clicked.connect(self.doDeleteFileButton)
         clearListButton.clicked.connect(self.doClearListButton)
-        
+
         topLayout = QHBoxLayout()
         topLayout.addWidget(openFileButton)
         topLayout.addWidget(deleteFileButton)
         topLayout.addWidget(clearListButton)
         bottomLayout = QHBoxLayout()
         bottomLayout.addWidget(self.fileListWidget)
-        mainLayout = QVBoxLayout() 
+        mainLayout = QVBoxLayout()
         mainLayout.addLayout(topLayout)
         mainLayout.addLayout(bottomLayout)
         self.setLayout(mainLayout)
 
-
     def dragEnterEvent(self, event):
         event.accept()
-    
-    
+
     def dropEvent(self, event):
         for eachFile in event.mimeData().urls():
             filePath = eachFile.toString()
@@ -53,52 +53,74 @@ class andysFileListWidget(QWidget):
                         if Path.is_file(file):
                             self.allFileListArray.append(file)
                             self.fileListWidget.addItem(file.name)
-        #print(self.allFileListArray)
-
+        print(len(self.fileListWidget))
+        print(len(self.allFileListArray))
 
     def dragMoveEvent(self, event):
         event.accept()
 
-
     def getCurrentRowFilePath(self):
+        print('getCurrentRowFilePath')
         return self.allFileListArray[self.fileListWidget.currentRow()]
-       
+        
+    def getThisRowFilePath(self, rowNumber):
+        # index start = 0
+        print('getThisRowFilePath')
+        if (rowNumber < len(self.allFileListArray) and len(self.allFileListArray) > 0):
+            return self.allFileListArray[rowNumber]
+        else:
+            return ''
 
-    # def doCurrentRowChanged(self):
-        # # print(self.fileListWidget.currentRow())
-        # # print(self.getCurrentRowFilePath())
-        # print(self.getCurrentRowFilePath())
-        
-        
+    def getCurrentRow(self):
+        print('getCurrentRow')
+        return self.fileListWidget.currentRow()
+
+    def getAllFileListArray(self):
+        print('getCurrentRowFilePath')
+        return self.allFileListArray
+
+    def doCurrentRowChanged(self):
+        print(len(self.fileListWidget))
+        #print(len(self.allFileListArray))
+        print(self.allFileListArray[self.fileListWidget.currentRow()])
+        #print(self.fileListWidget[)
+
     def doOpenFileButton(self):
+        print('doOpenFileButton')
         # 打开文件。如果是文件夹请拖拽。
-        tempFileName = QFileDialog.getOpenFileNames(self,  "打开文件", ".", "*.*")[0]
+        tempFileName = QFileDialog.getOpenFileNames(self, "打开文件", ".", "*.*")[0]
         for eachFilePath in tempFileName:
             if (Path(eachFilePath) not in self.allFileListArray):
-                    if Path.is_file(Path(eachFilePath)):
-                        self.allFileListArray.append(Path(eachFilePath))
-                        self.fileListWidget.addItem(Path(eachFilePath).name)
-                    if Path.is_dir(Path(eachFilePath)):
-                        for eacheachFilePath in Path(eachFilePath).glob('**/*'):
-                            if Path.is_file(eacheachFilePath):
-                                self.allFileListArray.append(eacheachFilePath)
-                                self.fileListWidget.addItem(eacheachFilePath.name)
-
-    
+                if Path.is_file(Path(eachFilePath)):
+                    self.allFileListArray.append(Path(eachFilePath))
+                    self.fileListWidget.addItem(Path(eachFilePath).name)
+                if Path.is_dir(Path(eachFilePath)):
+                    for eacheachFilePath in Path(eachFilePath).glob('**/*'):
+                        if Path.is_file(eacheachFilePath):
+                            self.allFileListArray.append(eacheachFilePath)
+                            self.fileListWidget.addItem(eacheachFilePath.name)
+        print(len(self.fileListWidget))
+        print(len(self.allFileListArray))
+        
     def doDeleteFileButton(self):
+        print('doDeleteFileButton')
+        print(self.getThisRowFilePath(1))
         if (self.fileListWidget.currentRow() > -1):
             self.fileListWidget.takeItem(self.fileListWidget.row(self.fileListWidget.currentItem()))
             self.allFileListArray.remove(self.allFileListArray[self.fileListWidget.currentRow()])
-    
+        print(len(self.fileListWidget))
+        print(len(self.allFileListArray))
+
     def doClearListButton(self):
+        print('doClearListButton')
         self.fileListWidget.clear()
         self.allFileListArray = []
         # print(self.fileListWidget.currentRow())
         # print(self.allFileListArray)
-    
-        
-# if __name__ == '__main__':
-    # app = QApplication(sys.argv)
-    # mywidget = andysFileListWidget()
-    # mywidget.show()
-    # sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mywidget = andysFileListWidget()
+    mywidget.show()
+    sys.exit(app.exec_())
